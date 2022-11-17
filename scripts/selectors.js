36047ecc9f4e46b6212ea2d033b4a14f212ded26
Fallencorex <absorb7000@gmail.com>
2022-11-17 06:10:29 +0200

    Another selector has been added +icon from selects

--- scripts/selectors.js
+++ scripts/selectors.js
@@ -4,8 +4,11 @@ class Selectors {
     // Method that will find all elements with the select tag on the page, hide them,
     // and create a custom select according to the layout, in place of the standard select.
     createSelectors(takenSelectors) {
+        let counter = takenSelectors.length;
+        
         // Go through all the selects, hide them, and count the number of their options.
         for (let elem of takenSelectors) {
+            console.log(counter);
             const nameOfParent = elem.parentNode.className;
             const defValueOfSelect = elem.value;
             let totalOptions = 0;
@@ -16,10 +19,16 @@ class Selectors {
             // Creating a custom select
             const newSelector = document.createElement("div");
             newSelector.className = "select-wrapper__select";
+            // Select should not overlap each other
+            newSelector.style.zIndex = counter;
             
             const headSelector = document.createElement("div");
             headSelector.className = "select__head";
             newSelector.appendChild(headSelector);
+
+            const shapeImage = document.createElement("img");
+            shapeImage.src = "./resources/images/shape.png";
+            newSelector.appendChild(shapeImage);
 
             const pickArea = document.createElement("div");
             pickArea.textContent = defValueOfSelect;
@@ -39,10 +48,12 @@ class Selectors {
             const appendSelector = document.getElementsByClassName(nameOfParent);
             appendSelector[0].appendChild(newSelector);
             totalOptions = 0;
+            counter--;
         }
     }
 }
 
-const select1 = new Selectors();
 
+const select1 = new Selectors();
 select1.createSelectors(document.getElementsByTagName("select"));
+
